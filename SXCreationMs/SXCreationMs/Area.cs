4ed7ccb43f6fdf982ee3bd3eb3ac101a485d960b
liu1054593535 <854046222@qq.com>
2019-05-16 20:04:01 +0800

    添加Area(行政区划实体)类文件

---
+++ SXCreationMs/SXCreationMs/Area.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SXCreationMs
+{
+    public class Area
+    {
+        public string AreaCode { get; set; }
+    }
+}
